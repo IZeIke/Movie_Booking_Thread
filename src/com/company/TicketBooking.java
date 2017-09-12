daa796faaa9e547590380eade0c983ce41df8b7b
eveem <monpriya.tammavong@gmail.com>
2017-09-12 15:58:12 +0700

    Add poolsThread and Executor

--- src/com/company/TicketBooking.java
+++ src/com/company/TicketBooking.java
@@ -1,6 +1,12 @@
 package com.company;
 
 import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TicketBooking {
 
@@ -13,76 +19,58 @@ public class TicketBooking {
         Movie m5 = new Movie();
         Movie[] MovieList = {m1, m2, m3, m4, m5};
 
+        List<Future<String>> futures = new ArrayList<Future<String>>(100);
 
-        Thread user1 = new Thread (new User("user1", MovieList));
-        user1.start();
+        ExecutorService pool = Executors.newFixedThreadPool(5);
+        while (true) {
+            pool.submit(new User(MovieList));
+        }
 
-        Thread user2 = new Thread (new User("user2", MovieList));
-        user2.start();
 
-        Thread user3 = new Thread (new User("user3", MovieList));
-        user3.start();
-
-        Thread user4 = new Thread (new User("user4", MovieList));
-        user4.start();
-
-        Thread user5 = new Thread (new User("user5", MovieList));
-        user5.start();
 
     }
 }
 
+class User implements Callable<String> {
 
-class User extends Thread {
-
-    String Username;
+    String[] AllUser = {"user1", "user2", "user3", "user4", "user5"};
     Movie[] MovieList;
 
-    public User(String name, Movie[] list)
-    {
-        Username = name;
-        MovieList = list;
+    public User (Movie[] movieLists) {
+        MovieList = movieLists;
     }
 
-    Random randMovie = new Random();
-    Random randTicket = new Random();
-    Random decision = new Random();
+    public String call() {
+        Random randUser = new Random();
+        Random randMovie = new Random();
+        Random randTicket = new Random();
+        Random decision = new Random();
 
-    @Override
-    public synchronized void run() {
-        while(true) {
+        String Username = AllUser[randUser.nextInt(5)];
 
-            int NumM = randMovie.nextInt(5);
-            int NumT = randTicket.nextInt(5) + 1;
-            MovieList[NumM].TicketNumber -= NumT;
+        int NumM = randMovie.nextInt(5);
+        int NumT = randTicket.nextInt(5) + 1;
+        MovieList[NumM].TicketNumber -= NumT;
 
+        int Numdecision = decision.nextInt(2);
 
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            int Numdecision = decision.nextInt(2);
-
-            if (Numdecision == 0) {
-                MovieList[NumM].TicketNumber += NumT;
-                System.out.println(Username + " deny Movie" + NumM + " return " + NumT + " ticket");
-            } else {
-                if (MovieList[NumM].getTicketNumber() < 0) {
-                    System.out.println(Username + " can not book ticket because this seat booked");
-                    MovieList[NumM].TicketNumber = 0;
-                } else {
-                    System.out.println(Username + " watch Movie" + NumM + " with " + NumT + " ticket");
-                }
-            }
-
+        if (Numdecision == 0) {
+            MovieList[NumM].TicketNumber += NumT;
+            System.out.println(Username + " deny Movie" + NumM + " return " + NumT + " ticket");
+        } else {
             if (MovieList[NumM].getTicketNumber() < 0) {
+                System.out.println(Username + " can not book ticket because this seat booked");
                 MovieList[NumM].TicketNumber = 0;
+            } else {
+                System.out.println(Username + " watch Movie" + NumM + " with " + NumT + " ticket");
             }
-
-            System.out.println("Movie" + NumM + " have " + MovieList[NumM].getTicketNumber() + " tickets");
         }
 
+        if (MovieList[NumM].getTicketNumber() < 0) {
+            MovieList[NumM].TicketNumber = 0;
+        }
+        System.out.println("Movie" + NumM + " have " + MovieList[NumM].getTicketNumber() + " tickets");
+        return ("Movie" + NumM + " have " + MovieList[NumM].getTicketNumber() + " tickets");
     }
+
 }
